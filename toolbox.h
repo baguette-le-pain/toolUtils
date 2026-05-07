491a14bd73060c9b7bafdae2bad43c620df2d8dc
baguette-le-pain <monastyrovyurii@gmail.com>
2026-05-07 07:08:40 -0400

    add PACKED macro and rename defer.h to toolbox.h

--- toolbox.h
+++ toolbox.h
@@ -1,3 +1,4 @@
+// defer
 #define CONCAT_INTERNAL(x, y) x##y
 #define CONCAT(x, y) CONCAT_INTERNAL(x, y)
 
@@ -8,3 +9,6 @@ static inline void _defer_cleanup(void (**fn)(void)) {
 #define defer(code) \
     void CONCAT(_defer_fn, __LINE__)(void) { code; } \
     void (*CONCAT(_defer_ptr, __LINE__)) __attribute__((cleanup(_defer_cleanup))) = CONCAT(_defer_fn, __LINE__)
+
+//packing
+#define PACKED __attribute__((packed))
